c27991bd743507f732be0418a4f495d0ca322e6b
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-02-03 20:20:55 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://tribebuddy.com/',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
