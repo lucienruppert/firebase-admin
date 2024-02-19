1e1a368675b5664a3bfb806d18fbadadad822870
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-02-19 16:54:16 +0100

    refactor two services into one

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { FirebaseModule } from 'src/modules/users/users.service';
 
 @Module({
-  imports: [FirebaseModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
