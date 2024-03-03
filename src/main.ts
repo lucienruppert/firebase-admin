f22cd22b1a4c10ae6caa9327d9a0c784d8364697
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-03-03 14:33:57 +0100

    env test

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,5 @@ async function bootstrap(): Promise<void> {
     origin: ['https://tribebuddy.com', 'http://localhost:4200'],
   });
   await app.listen(process.env.PORT || 3000);
-  console.log('here is the variable:', process.env.BLABLA);
 }
 bootstrap();
